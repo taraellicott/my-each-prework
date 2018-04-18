4115870cca060a777775710e7e2c1dfb46944e26
Tara Ellicott <taraellicott@gmail.com>
2018-04-18 03:54:57 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,8 @@
-def my_each(collection)
+def my_each(collection) 
   i = 0
   while i < collection.length
-  puts collection
-end
-
-
-my_each(collection) do |i|
-  puts i
-end
-
-i = 0
-  while i < collection.length
     yield(collection[i])
-    i = i + 1
+    i+=
   end
   collection
 end
